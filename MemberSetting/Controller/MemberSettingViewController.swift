46a0dfb6593d9d5a65f0d6409360b16cf99209b6
Willy Hsu <hsunefer@gmail.com>
2026-07-13 10:53:03 +0800

    refactor: 整理 Base Root 目錄結構並新增 MemberSetting 模組骨架

---
+++ MemberSetting/Controller/MemberSettingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MemberSettingViewController.swift
+//  MyTMDB_App
+//
+//  Created by Willy Hsu on 2026/7/13.
+//
+
+import SnapKit
+import UIKit
+
+@MainActor
+final class MemberSettingViewController: BaseListViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
